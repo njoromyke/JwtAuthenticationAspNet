6fe678f7a342e2d7e014fa73482e489484d029ab
njoromyke <mykenjoroge01@gmail.com>
2024-01-21 11:29:31 +0300

    add login

--- JwtAuthenticationAspNet/Controllers/AuthController.cs
+++ JwtAuthenticationAspNet/Controllers/AuthController.cs
@@ -1,8 +1,13 @@
-﻿using JwtAuthenticationAspNet.Core.Dtos;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtAuthenticationAspNet.Core.Dtos;
 using JwtAuthenticationAspNet.Core.OtherObjects;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JwtAuthenticationAspNet.Controllers
 {
@@ -10,16 +15,16 @@ namespace JwtAuthenticationAspNet.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-       private readonly UserManager<IdentityUser> _userManager;
-       private readonly RoleManager<IdentityRole> _roleManager;
-       private readonly IConfiguration _configuration;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
 
-       public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
-       {
-           _userManager = userManager;
-           _roleManager = roleManager;
-           _configuration = configuration;
-       }
+        public AuthController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
 
         //Route For Seeding Roles
         [HttpPost]
@@ -82,6 +87,63 @@ namespace JwtAuthenticationAspNet.Controllers
             await _userManager.AddToRoleAsync(user, StaticUserRoles.User);
 
             return Ok("User Created Successfully");
+        }
+
+        //Route -> Login
+        [HttpPost]
+        [Route("login")]
+
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.UserName);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid Credentials");
+            }
+
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+
+            if (!isPasswordValid)
+            {
+                return Unauthorized("Invalid Credentials");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var authClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim("JwtId", Guid.NewGuid().ToString()),
+            };
+
+            foreach (var userRole in userRoles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
+
+            var token = GenerateNewJsonWebToken(authClaims);
+
+            return Ok(token);
+        }
+
+        private string GenerateNewJsonWebToken(List<Claim> claims)
+        {
+            var authSecret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var tokenObject = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(1),
+                claims: claims,
+                signingCredentials: new SigningCredentials(authSecret, SecurityAlgorithms.HmacSha256)
+                );
+
+            string token = new JwtSecurityTokenHandler().WriteToken(tokenObject);
+
+            return token;
+
         }
     }
 }
